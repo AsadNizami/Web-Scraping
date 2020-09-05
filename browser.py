7c2e560b30184fe76e277606ca01c307de667405
Asad <asadnizami123@gmail.com>
2020-09-06 03:58:29 +0530

    'test'

--- browser.py
+++ browser.py
@@ -13,7 +13,8 @@ def save(file_name, data):
     file_name_mod = file_name.split('.')[0].strip('https://')
     page_dir = os.path.join(directory, file_name_mod)
     with open(page_dir, 'w', encoding='utf-8') as file_in:
-        file_in.write(Fore.BLUE + data)
+        # file_in.write(Fore.BLUE + data)
+        file_in.write(data)
     with open(page_dir, 'r', encoding='utf-8') as file_out:
         for line in file_out:
             print(line)
@@ -22,11 +23,13 @@ def save(file_name, data):
 def browse(url):
     source_code = requests.get(url)
     soup = BeautifulSoup(source_code.content, 'html.parser')
-    # for i in range(20):
-    #     if '<script' in str(soup):
-    #         soup.script.decompose()
+    for i in range(20):
+        if 'script' in str(soup):
+            soup.script.decompose()
     # print(soup)
     return soup.get_text()
+    print(soup.replace('<script', ''))
+    return None
 
 
 def make_url(url):
